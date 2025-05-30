436969dd4b1909f8ec9e29867ac5d4b4af34f327
Satiro07 <jardelsatiro57@gmail.com>
2025-05-30 16:52:38 -0300

    Create ImparPar.java

---
+++ condicionais/ImparPar.java
@@ -0,0 +1,14 @@
+package condicionais;
+
+public class impar_par {
+    public static void main(String[] args) {
+        int numero = 4;
+        if (numero % 2 == 0) {
+            System.out.printf("O número '%d' é par!", numero);
+        }
+        else {
+            System.out.printf("O número '%d' é impar!", numero);
+        }
+    }
+    
+}
