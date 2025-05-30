78e8dab6a6a8b4d881d06c570b48495483a9064b
Satiro07 <jardelsatiro57@gmail.com>
2025-05-30 17:09:57 -0300

    Update MaiorMenor.java

--- condicionais/MaiorMenor.java
+++ condicionais/MaiorMenor.java
@@ -3,13 +3,13 @@ package condicionais;
 public class MaiorMenor{
     public static void main(String[] args) {
         double numero1 = 5.9;
-        double numero2 = 4.90;
+        double numero2 = 690;
 
         if (numero1 > numero2) {
             System.out.printf("O número '%.2f' é maior que o número '%.2f'", numero1, numero2);
         }
         else if (numero1 < numero2) {
-            System.out.printf("O número '%.2f' é maior que o número '%.2f'", numero2, numero1);
+            System.out.printf("O número '%.2f' é menor que o número '%.2f'", numero1, numero2);
         }
         else {
             System.out.printf("Os números são iguais -> número 1: '%.2f' e número 2: '%.2f'", numero1, numero2);
