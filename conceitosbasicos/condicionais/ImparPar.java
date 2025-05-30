b41633b1233cb62eb4be16f91505a407be6c53f5
Satiro07 <jardelsatiro57@gmail.com>
2025-05-30 18:15:51 -0300

    Create ImparPar.java

---
+++ conceitosbasicos/condicionais/ImparPar.java
@@ -0,0 +1,14 @@
+package conceitosbasicos.condicionais;
+
+public class ImparPar{
+    public static void main(String[] args) {
+        int numero = 4;
+        if (numero % 2 == 0) {
+            System.out.printf("O número '%d' é par!", numero);
+        }
+        else {
+            System.out.printf("O número '%d' é impar!", numero);
+        }
+    }
+    
+}
