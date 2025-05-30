f6c7cda291c0a4619bdaf34423dfc8c4191eb158
Satiro07 <jardelsatiro57@gmail.com>
2025-05-30 17:55:12 -0300

    Create CalculadoraSimples.java

---
+++ condicionais/CalculadoraSimples.java
@@ -0,0 +1,40 @@
+package condicionais;
+import java.util.Scanner;
+public class CalculadoraSimples {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        double numero1 = input.nextDouble();
+
+        System.out.println("Digite outro número:");
+        double numero2 = input.nextDouble();
+
+        System.out.println("Agora escolha uma operação [+, -, x, /]");
+        String operacao = input.next();
+
+        switch (operacao) {
+            case "+":
+                double soma = numero1 + numero2;
+                System.out.printf("%.2f + %.2f = %.2f", numero1, numero2, soma);                
+                break;
+            case "-":
+                double subtracao = numero1 - numero2;
+                System.out.printf("%.2f - %.2f = %.2f", numero1, numero2, subtracao); 
+                break;
+            case "x":
+                double multiplicacao = numero1 * numero2;
+                System.out.printf("%.2f x %.2f = %.2f", numero1, numero2, multiplicacao); 
+                break;
+            case "/":
+                double divisao = numero1 - numero2;
+                System.out.printf("%.2f / %.2f = %.2f", numero1, numero2, divisao); 
+                break;
+            default:
+                System.out.printf("Operação '%d' inválida!", operacao);
+                break;
+        }
+        input.close();
+    }
+}
