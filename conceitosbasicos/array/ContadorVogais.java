4baad8924642e95d055d808d88f196afd3d84b6a
Satiro07 <jardelsatiro57@gmail.com>
2025-05-31 19:40:16 -0300

    Create ContadorVogais.java

---
+++ conceitosbasicos/array/ContadorVogais.java
@@ -0,0 +1,17 @@
+package conceitosbasicos.array;
+import java.util.Scanner;
+
+public class ContadorVogais {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numeros = new int[10];
+        System.out.println(numeros.length);
+        
+    
+    }
+
+    // public static int lista(Scanner input) {
+    //     int[] numeros = new int[10];
+    //     System.out.println(numeros.length);
+    // }
+}
