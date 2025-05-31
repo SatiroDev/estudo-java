c456ea18934a883dd7cfef6af3a8ea55a6482bc9
Satiro07 <jardelsatiro57@gmail.com>
2025-05-31 17:26:51 -0300

    Create notas.java

---
+++ conceitosbasicos/array/notas.java
@@ -0,0 +1,37 @@
+package conceitosbasicos.array;
+import java.util.Scanner;
+public class notas {
+    public static void main(String[] args) {
+        double[] resp = add_notas(0);
+
+        int quantidade = resp.length;
+        System.out.println("Notas adicionadas:");
+        for (int i = 0; i < quantidade; i++) {
+            System.out.printf("Nota %d: %.2f%n", i+1, resp[i]);
+        }
+        System.out.printf("Média: %.2f", media(resp));
+    }
+
+    public static double[] add_notas(double entrada){
+        Scanner input = new Scanner(System.in);
+        double[] notas_aluno = new double[5]; 
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Digite a %d° nota: ", i+1);
+            double nota = input.nextDouble();
+            notas_aluno[i] = nota;
+        }
+        System.out.println();
+        input.close();
+        return notas_aluno;
+    }
+
+    public static double media(double[] notas) {
+        double soma = 0;
+        for (int i = 0; i < 5; i++) {
+            soma += notas[i];
+        }
+        double media = soma / notas.length;
+        return media;
+    }
+    
+}
