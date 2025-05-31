2d59754e88d9f2768c35f629202aa66b0c6ace6a
Satiro07 <jardelsatiro57@gmail.com>
2025-05-31 20:09:29 -0300

    Create MaiorMenor.java

---
+++ conceitosbasicos/array/MaiorMenor.java
@@ -0,0 +1,34 @@
+package conceitosbasicos.array;
+import java.util.Scanner;
+
+public class ContadorVogais {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] resp = lista(input);
+        int maior_num = maior_numero(resp);
+        System.out.printf("O maior número adicionado foi %d", maior_num);
+        input.close();
+    
+    }
+
+    public static int[] lista(Scanner input) {
+        System.out.println("Digite números inteiros:");
+        int[] numeros = new int[10];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Digite o %d° número: ",i+1);
+            int numero = input.nextInt();
+            numeros[i] = numero;
+        }
+        return numeros;
+    }
+
+    public static int maior_numero(int[] lista) {
+        int maior = 0;
+        for (int n: lista) {
+            if (n >= maior) {
+                maior = n;
+            }
+        }
+        return maior;
+    }
+}
