6398e525bd8909fe1a88d6598115f8e62b1c4cf1
Satiro07 <jardelsatiro57@gmail.com>
2025-05-31 17:32:26 -0300

    Update notas.java

--- conceitosbasicos/array/notas.java
+++ conceitosbasicos/array/notas.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class notas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double[] resp = add_notas(0);
+        double[] resp = add_notas(input);
 
         int quantidade = resp.length;
         System.out.println("Notas adicionadas:");
@@ -15,7 +15,7 @@ public class notas {
         input.close();
     }
 
-    public static double[] add_notas(double entrada){
+    public static double[] add_notas(Scanner input){
         double[] notas_aluno = new double[5]; 
         for (int i = 0; i < 5; i++) {
             System.out.printf("Digite a %d° nota: ", i+1);
@@ -28,7 +28,7 @@ public class notas {
 
     public static double media(double[] notas) {
         double soma = 0;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < notas.length; i++) {
             soma += notas[i];
         }
         double media = soma / notas.length;
