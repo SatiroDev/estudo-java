0c836f6ab1803bc559e1b06d3a42534fa54d29dd
Satiro07 <jardelsatiro57@gmail.com>
2025-05-31 21:21:08 -0300

    Update InverterArray.java

--- conceitosbasicos/array/InverterArray.java
+++ conceitosbasicos/array/InverterArray.java
@@ -4,8 +4,9 @@ public class InverterArray {
     public static void main(String[] args) {
         int[] lista = {1, 2, 3, 4, 5};
         int[] nova = inverterLista(lista);
-        for (int n: nova) {
-            System.out.println(n);
+        System.out.println("A lista invertida:");
+        for (int n: nova) { 
+            System.out.printf("%d ", n);
         }
 
     }
@@ -13,8 +14,9 @@ public class InverterArray {
     public static int[] inverterLista(int[] numeros) {
         int[] nova_lista = new int[numeros.length];
         int contagem = 0;
-        for (int i = numeros.length; i >=0; i--) {
+        for (int i = numeros.length-1; i >=0; i--) {
             nova_lista[contagem] = numeros[i];
+            contagem ++;
         }
         return nova_lista;
     }
