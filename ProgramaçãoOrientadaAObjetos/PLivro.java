bce7c3f729b25c952cda941a9002a2e49d7f2a6f
Satiro07 <jardelsatiro57@gmail.com>
2025-06-01 21:53:27 -0300

    Create PLivro.java

---
+++ ProgramaçãoOrientadaAObjetos/PLivro.java
@@ -0,0 +1,52 @@
+package ProgramaçãoOrientadaAObjetos;
+
+public class PLivro {
+    public static void main(String[] args) {
+        Livro l = new Livro();
+        l.titulo = "Dom Casmusso";
+        l.autor = "Machado de Assis";
+        l.quantidade_de_paginas = 256;
+        l.pagina_atual = 1;
+
+        l.motrarDetalhesLivro();
+        l.avancarPagina();
+        l.motrarDetalhesLivro();
+        l.voltarPagina();
+        l.motrarDetalhesLivro();
+
+    }
+}
+
+class Livro {
+    String titulo;
+    String autor;
+    int quantidade_de_paginas;
+    int pagina_atual;
+
+    void avancarPagina() {
+        if (pagina_atual < quantidade_de_paginas) {
+            pagina_atual ++;
+        }
+        else {
+            System.out.println("Você ja chegou no fim do livro! Não tem mais páginas para avançar!");
+        }
+    }
+
+    void voltarPagina() {
+        if (pagina_atual > 1) {
+            pagina_atual --;
+        }
+        else {
+            System.out.println("Você está na primeira página do livro! Não tem páaginas para voltar!");
+        }
+    }
+
+    void motrarDetalhesLivro() {
+        System.out.println("Detalhes:");
+        System.out.printf("Título do livro: %s%n", titulo);
+        System.out.printf("Autor do livro: %s%n", autor);
+        System.out.printf("Total de páginas: %d%n", quantidade_de_paginas);
+        System.out.printf("Página atual: %d%n%n", pagina_atual);
+        
+    }
+}
