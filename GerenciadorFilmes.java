4225a02118920195f4d585c8c2b7293ede5ae6ef
Satiro07 <jardelsatiro57@gmail.com>
2025-06-01 22:22:56 -0300

    Update GerenciadorFilmes.java

---
+++ GerenciadorFilmes.java
@@ -0,0 +1,48 @@
+public class GerenciadorFilmes {
+    public static void main(String[] args) {
+        Filme f = new Filme();
+        f.titulo = "A Origem";
+        f.diretor = "Christopher Nolan";
+        f.ano_de_lancamento = 2010;
+        f.duracao_em_minutos = 148;
+
+        f.mostrarDetalhes();
+        f.avaliar(8.5);
+        f.avaliar(9.0);
+        f.mostrarDetalhes();
+        System.out.println(f.ehClassico());
+
+
+    }
+}
+
+class Filme {
+    String titulo;
+    String diretor;
+    int ano_de_lancamento;
+    int duracao_em_minutos;
+    double avaliacao_media = 0;
+    int total_de_avaliacoes = 0;
+
+    void avaliar(double nota) {
+        if (nota >= 0 && nota <= 10) {
+            total_de_avaliacoes ++;
+            avaliacao_media = (avaliacao_media + nota) / total_de_avaliacoes;
+        }
+        else {
+            System.out.println("Nota inválida! Aceitamos notas de 0 a 10!");
+        }
+    }
+    void mostrarDetalhes() {
+        System.out.printf("Título do filme: %s%n", titulo);
+        System.out.printf("Diretor do filme: %s%n", diretor);
+        System.out.printf("Ano de lançamento do filme: %d%n", ano_de_lancamento);
+        System.out.printf("Duração em minutos do filme: %d%n",duracao_em_minutos);
+        System.out.printf("Avaliação média do filme: %.2f%n", avaliacao_media);
+        System.out.printf("Total de avaliações do filme: %d%n%n", total_de_avaliacoes);
+    }
+
+    boolean ehClassico() {
+        return 2025 - ano_de_lancamento > 25;
+    }
+}
