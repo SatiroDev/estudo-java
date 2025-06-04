e062beec6bd71822f5e5b4bd2bbc6fc612b6f778
Satiro07 <jardelsatiro57@gmail.com>
2025-06-04 19:29:06 -0300

    .

---
+++ POOAvanc/GerenciamentoCompras.java
@@ -0,0 +1,19 @@
+package POOAvanc;
+
+public class GerenciamentoCompras {
+    public static void main(String[] args) {
+        GCProduto produto = new GCProduto("Caderno", 15.5, 3);
+        produto.exibirInformacoesProduto();
+
+        GCProduto produto1 = new GCProduto("Borracha", 2, 5);
+        produto1.exibirInformacoesProduto();
+
+        GCCarrinho carrinho = new GCCarrinho();
+        carrinho.adicionarProduto((produto));
+
+        carrinho.adicionarProduto(produto1);
+
+        carrinho.calcularValorTotal();
+
+    }
+}
