ff9e5843ae54d9c27a93b25022a7b8e6611d5dcf
Satiro07 <jardelsatiro57@gmail.com>
2025-06-04 19:29:22 -0300

    .

---
+++ POOAvanc/GCCarrinho.java
@@ -0,0 +1,52 @@
+package POOAvanc;
+
+public class GCCarrinho {
+    private GCProduto[] produtos = new GCProduto[200];
+    private int indice = 0;
+    private double valorTotal = 0;
+
+    public GCCarrinho() {
+
+    }
+
+    public void adicionarProduto (GCProduto produto) {
+        if (indice < produtos.length) {
+            this.produtos[indice] = produto;
+            indice ++;
+        }
+        else {
+            System.out.println("Carrinho cheio!");
+        }
+    }
+
+    public void calcularValorTotal () {
+        for (GCProduto produto: produtos) {
+            if (produto != null) {
+                double total = produto.preco * produto.estoque;
+                valorTotal += total;
+            }
+            else {
+                System.out.println("Valor total: " + valorTotal);
+                break;
+            }
+
+        }
+
+    }
+
+    public void exibirProdutos () {
+        int cont = 0;
+        for (GCProduto produto: produtos) {
+            if (cont < indice) {
+                System.out.println("Produto: " + produto.nome);
+                System.out.println("Preço: " + produto.preco);
+                System.out.println("Quantidade em estoque: " + produto.estoque);
+                cont ++;
+            }
+            else {
+                return;
+            }
+        }
+
+    }
+}
