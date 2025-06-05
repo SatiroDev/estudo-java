db5307f464d7f217d24b709f05b10c15a03bcd51
Satiro07 <jardelsatiro57@gmail.com>
2025-06-04 21:35:37 -0300

    .

--- POOAvanc/SistemaBiblioteca/SBUsuario.java
+++ POOAvanc/SistemaBiblioteca/SBUsuario.java
@@ -3,7 +3,6 @@ package POOAvanc.SistemaBiblioteca;
 public class SBUsuario {
     private String nome;
     private SBLivro[] livrosEmprestados = new SBLivro[3];
-    private int quantidade_livros = 0;
     private int indice = 0;
 
     public SBUsuario (String nome) {
@@ -11,21 +10,20 @@ public class SBUsuario {
     }
 
     public void emprestarLivro(SBLivro livro) {
-        if (livro.getQuantidadeDisponivel() > 0 && quantidade_livros < 3) {
+        if (livro.getQuantidadeDisponivel() > 0 && indice < 3) {
             livrosEmprestados[indice] = livro;
             livro.setQuantidadeDisponivel(livro.getQuantidadeDisponivel() - 1);
             indice ++;
         }
+        else {
+            System.out.println("Erro! Quantidade de livro indisponivel ou você atingiu o limite de livros emprestado");
+        }
     }
 
     public void exibirLivrosEmprestados() {
-        int cont = 0;
+        System.out.println("Livros emprestados:");
         for (SBLivro livro: livrosEmprestados) {
             if (livro != null) {
-                if (cont == 0) {
-                    System.out.println("Livros emprestados:");
-                    cont ++;
-                }
                 System.out.println("Titulo: " + livro.getTitulo());
             }
         }
