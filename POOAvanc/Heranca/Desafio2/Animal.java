2d89d434d064839702020e7134939610f8d493af
Satiro07 <jardelsatiro57@gmail.com>
2025-06-06 19:55:29 -0300

    .

---
+++ POOAvanc/Heranca/Desafio2/Animal.java
@@ -0,0 +1,11 @@
+package POOAvanc.Heranca.Desafio2;
+
+public class Animal {
+    String nome;
+    int idade;
+
+    public void exibirDados() {
+         System.out.println("Nome: " + nome);
+         System.out.println("Idade: " + idade);
+     }
+}
