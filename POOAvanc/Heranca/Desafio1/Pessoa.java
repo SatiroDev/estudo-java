43d0df09e494fe35931518eb14f8edba4ced6dfd
Satiro07 <jardelsatiro57@gmail.com>
2025-06-06 19:23:24 -0300

    .

---
+++ POOAvanc/Heranca/Desafio1/Pessoa.java
@@ -0,0 +1,12 @@
+package POOAvanc.Heranca.Desafio1;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
+
